dc31b57b678d14705d77016d54b51769cc5d5fd0
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-04-06 20:03:07 -0300

    thread safe logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::io::{LineWriter, stderr, Write};
 use std::fs::OpenOptions;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 struct MyLogger {
     out_stream: Mutex<Box<dyn Write + Send>>
@@ -33,10 +35,22 @@ impl MyLogger {
 
 fn main() {
     let logger = &mut MyLogger::init("file_name.txt");
-    logger.log("En el thread main\n");
 
+    let handle;
+    {
+        let logger = logger.clone();
+        handle = thread::spawn(move || {
+            for i in 1..5 {
+                logger.log(format!("hi number {} from the spawned thread!\n", i).as_str());
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+    }
 
+    for i in 1..5 {
+        logger.log(format!("hi number {} from the main thread!\n", i).as_str());
+        thread::sleep(Duration::from_millis(1));
+    }
 
-
-
+    handle.join().unwrap();
 }
